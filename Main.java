768b740d42faedaa03693c58e0164856d268eb15
nmurray <nmurray@atl.year.org>
2015-10-23 11:02:21 -0400

    LargestNumber

---
+++ Main.java
@@ -0,0 +1,31 @@
+﻿
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# On branch master
+# Your branch is up-to-date with 'origin/master'.
+#
+# Changes to be committed:
+#	new file:   Main.java
+#	new file:   Person.java
+#
+# Untracked files:
+#	../../.metadata/
+#	../../.recommenders/
+#	../../Circle/
+#	../.classpath
+#	../.gitignore
+#	../.project
+#	../.settings/
+#	../../Practice Labs/
+#	../../Project1/
+#	../../Project2/
+#	../../Tip Calculator/.classpath
+#	../../Tip Calculator/.gitignore
+#	../../Tip Calculator/.project
+#	../../Tip Calculator/.settings/
+#	../../TipCalculator/
+#	../../hello.txt
+#	../../in5gxnau.kun.txt
+#	../../pipeitup.txt
+#
+
---
+++ Practice Labs/src/Practice.java
@@ -0,0 +1,13 @@
+
+public class Practice {
+public static void main(String [] args)
+{
+int x = 5;
+double y = 12.5;
+String s = "Java Programming";
+System.out.println(x);
+System.out.println(y);
+System.out.println(s);
+	
+}
+}
