fac36eb1cab9c4048b3ae60be31c55502d407d6e
nmurray <nmurray@atl.year.org>
2015-11-06 08:04:57 -0500

    Palindrome

--- Palindrome/src/Palindrome.java
+++ Palindrome/src/Palindrome.java
@@ -1,34 +1,36 @@
 import java.util.Scanner;
 
-public class Palindrome {
-
+public class Palindrome 
+{
+	
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-		
-		String inputNoSpaces = input.replace(" ",  "");
-		PalindromeCheck(inputNoSpaces);
-		
-sc.close();
+
+		String inputNoSpaces = input.replace(" ", "");
+				PalindromeCheck(inputNoSpaces);
 	}
-	
-private static void PalindromeCheck(String input)
-{
-	if (input !=null)
+
+	private static void PalindromeCheck (String input)
 	{
-		char[] inputChars = input.toCharArray();
-		boolean isPalindrome = true;
-		
-		for (int i = 0, j = inputChars.length - 1; i < inputChars.length/2; i++, j--)
+		if (input != null)	
 		{
-		if(inputChars[i] != inputChars[j])
-		{
-			isPalindrome = false;
-			break;
+			char[] inputChars = input.toCharArray();
+			boolean isPalindrome = true;
+
+			for(int i = 0, j = inputChars.length -1; i < inputChars.length/2; i++, j--)	
+			{
+				if (inputChars[i] != inputChars [j])
+				{
+					isPalindrome = false;
+							break;
+				}
+			}
+			
+			System.out.println(input + " is a palindrome " + isPalindrome);
 		}
-		}
-		System.out.println( input + " is a palindrome " + isPalindrome);
+
 	}
-}
+
 }
